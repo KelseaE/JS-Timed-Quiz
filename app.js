210e6ac4836ac66896bd1160b5ed08b9f231d367
KEDell <sosha98@live.com>
2023-12-15 22:40:57 -0500

    Questions added. Grammar fixed

--- app.js
+++ app.js
@@ -2,7 +2,32 @@ const questions = [
     {
         prompt: "Commonly used data types DO NOT include:",
         options: ["Strings", "Booleans", "Alerts", "Numbers"],
-        answer: "Alerts" }
+        answer: "Alerts" 
+    },
+
+    {
+        prompt: "The condition in an if/else statement is enclosed within _____.",
+        options: ["Quotes", "Curly Brackets", "Parentheses", "Square Brackets"],
+        answer: "Curly Brackets"
+    },
+
+    {
+        prompt: "Arrays in Javascript can be used to store _____.",
+        options: ["Numbers & Strings", "Other Arrays", "Booleans", "All Of The Above"],
+        answer: "All Of The Above"
+    },
+
+    {
+        prompt: "String values must be enclosed within _____ when being assigned to variables.",
+        options: ["Commas", "Curly Brackets", "Quotes", "Parantheses"],
+        answer: "Quotes"
+    },
+
+    {
+        prompt: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        options: ["JavaScript", "Terrminal/Bash", "For Loops", "Console.log"],
+        answer: "Console.log"
+    }
     ];
 
     console.log(questions)
